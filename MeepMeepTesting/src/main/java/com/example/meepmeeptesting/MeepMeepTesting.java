24acfce892df049c7fda685e13a29c9e830ceef5
Jonathan Jones <jonathan.d.jones431@gmail.com>
2023-11-01 15:28:52 -0700

    Finished MeepMeep plan

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -9,22 +9,38 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(700);
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+        RoadRunnerBotEntity redAlliance = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(11, -70, Math.toRadians(90)))
                                 .waitSeconds(7)
-                                .turn(Math.toRadians(-40))
-                                .forward(40)
-                                .turn(Math.toRadians(-50))
+                                .forward(35)
+                                .turn(Math.toRadians(-90))
+                                .forward(35)
+                                .waitSeconds(8)
+                                .turn(Math.toRadians(-70))
+                                .forward(30)
+                                .turn(Math.toRadians(-110))
+                                .back(5)
                                 .build()
                 );
+
+        RoadRunnerBotEntity redAudience = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-35, -70, Math.toRadians(90)))
+                                .waitSeconds(7)
+                                .forward(35)
+                                .waitSeconds(8)
+                                .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
+                .addEntity(redAudience)
+                .addEntity(redAlliance)
                 .start();
     }
 }
