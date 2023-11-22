1ed75ecc8048440d91ba8df15cf3b19abe4a212b
Jonathan Jones <jonathan.d.jones431@gmail.com>
2023-11-22 14:40:42 -0700

    ServoConstants fix from home

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnicalTerrors/ServoTestConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnicalTerrors/ServoTestConstants.java
@@ -4,8 +4,8 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class ServoTestConstants {
-    public static final int arm1Pos = 0;
-    public static final int arm2Pos = 0;
-    public static final int wristPos = 0;
-    public static final int clawPos = 0;
+    public static int arm1Pos = 0;
+    public static int arm2Pos = 0;
+    public static int wristPos = 0;
+    public static int clawPos = 0;
 }
