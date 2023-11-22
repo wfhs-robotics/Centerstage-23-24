db2223e0b007586e2cbf3b8f4350d7004fe489ab
Jonathan Jones <jonathan.d.jones431@gmail.com>
2023-11-22 14:20:12 -0700

    ServoTest from home

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnicalTerrors/ServoTestConstants.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.TechnicalTerrors;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class ServoTestConstants {
+    public static final int arm1Pos = 0;
+    public static final int arm2Pos = 0;
+    public static final int wristPos = 0;
+    public static final int clawPos = 0;
+}
